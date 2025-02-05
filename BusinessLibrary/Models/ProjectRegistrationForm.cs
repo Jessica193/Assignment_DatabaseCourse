388581a16e91a8c81b28af112e4784772db87032
Jessica Eriksson <jessicahlqvist@hotmail.com>
2025-02-05 16:39:15 +0100

    Gjort färdigt Datalagret

---
+++ BusinessLibrary/Models/ProjectRegistrationForm.cs
@@ -0,0 +1,24 @@
+﻿using Data.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessLibrary.Models
+{
+    public class ProjectRegistrationForm
+    {
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Name must be at least two characters long.")]
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public DateTime EndDate { get; set; }
+
+        
+    }
+}
--- Data/Repositories/ProjectRepository.cs
+++ Data/Repositories/ProjectRepository.cs
@@ -1,9 +1,30 @@
 ﻿using Data.Contexts;
 using Data.Entities;
 using Data.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
 
 namespace Data.Repositories;
 
 public class ProjectRepository(DataContext context) : BaseRepository<ProjectEntity>(context), IProjectRepository
 {
+    public override Task<IEnumerable<ProjectEntity>> GetAllWithDetailsAsync(Func<IQueryable<ProjectEntity>, IQueryable<ProjectEntity>> includeExpression)
+    {
+        var projectEntities = _context.Projects
+            .Include(p => p.Customer)
+            .ThenInclude(c => c.ContactPerson)
+            .Include(p => p.Employee)
+            .ThenInclude(e => e.Role)
+            .Include(p => p.Service)
+            .ThenInclude(s => s.Unit)
+            .Include(p => p.StatusType)
+            .ToListAsync();
+
+        return projectEntities;
+    }
+
+    public override Task<ProjectEntity> GetOneWithDetailsAsync(Func<IQueryable<ProjectEntity>, IQueryable<ProjectEntity>> includeExpression, Expression<Func<ProjectEntity, bool>> predicate)
+    {
+        return base.GetOneWithDetailsAsync(includeExpression, predicate);
+    }
 }
