b4cb5574e6a19df140ee1e04578b67847bfc5382
Jessica Eriksson <jessicahlqvist@hotmail.com>
2025-02-12 16:37:26 +0100

    Presentation.ConsoleApp added. Not done.

---
+++ Presentation.ConsoleApp/Dialogs/MenuDialogs.cs
@@ -0,0 +1,118 @@
+﻿using Presentation.ConsoleApp.Interfaces;
+
+namespace Presentation.ConsoleApp.Dialogs;
+
+public class MenuDialogs : IMenuDialogs
+{
+    public void Run()
+    {
+        while (true)
+        {
+            Console.Clear();
+            var option = MainMenu();
+            if (!string.IsNullOrEmpty(option))
+            {
+                OptionSwitch(option);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("You must enter an option");
+            }
+        }
+    }
+
+    public string MainMenu()
+    {
+        
+            Console.Clear();
+            Console.WriteLine("**** MAIN MENU *****");
+            Console.WriteLine("");
+            Console.WriteLine("1. Create a project");
+            Console.WriteLine("2. View all projects");
+            Console.WriteLine("3. View one project");
+            Console.WriteLine("4. Update project");
+            Console.WriteLine("5. Delete project");
+            Console.WriteLine("6. Quit application");
+            Console.WriteLine("----------------------------------------");
+            Console.Write("Enter your option: ");
+
+            var option = Console.ReadLine()!;
+            return option;
+      
+    }
+
+    public void OptionSwitch(string option)
+    {
+        switch (option)
+        {
+            case "1":
+                CreateProject();
+                break;
+            case "2":
+                ViewAllProjects();
+                break;
+            case "3":
+                ViewOneProject();
+                break;
+            case "4":
+                UpdateProject();
+                break;
+            case "5":
+                DeleteProject();
+                break;
+            case "6":
+                QuitApplication();
+                break;
+            default:
+                Console.Clear();
+                Console.WriteLine("You must enter a valid option");
+                Console.ReadKey();
+                break;
+        }
+    }
+
+    public void CreateProject()
+    {
+        Console.Clear();
+        Console.WriteLine("Metod create");
+        Console.ReadKey();
+    }
+
+    public void ViewAllProjects()
+    {
+        Console.Clear();
+        Console.WriteLine("Metod view all");
+        Console.ReadKey();
+    }
+
+    public void ViewOneProject()
+    {
+        Console.Clear();
+        Console.WriteLine("Metod view one");
+        Console.ReadKey();
+    }
+
+    public void UpdateProject()
+    {
+        Console.Clear();
+        Console.WriteLine("Metod update");
+        Console.ReadKey();
+    }
+
+    public void DeleteProject()
+    {
+        Console.Clear();
+        Console.WriteLine("Metod delete");
+        Console.ReadKey();
+    }
+
+    public void QuitApplication()
+    {
+        Console.Clear();
+        Console.WriteLine("Metod quit");
+        Console.ReadKey();
+    }
+
+
+}
---
+++ Presentation.ConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using BusinessLibrary.Interfaces;
+using BusinessLibrary.Services;
+using Data.Contexts;
+using Data.Interfaces;
+using Data.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Presentation.ConsoleApp.Dialogs;
+using Presentation.ConsoleApp.Interfaces;
+using System.Security.Authentication.ExtendedProtection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var options = new JsonSerializerOptions()
+{
+    WriteIndented = true,
+    ReferenceHandler = ReferenceHandler.Preserve
+};
+
+var services = new ServiceCollection();
+
+services.AddDbContext<DataContext>(options => options.UseSqlite("Data Source=my_database.db"));
+
+services.AddScoped<IProjectRepository, ProjectRepository>();
+services.AddScoped<ICustomerRepository, CustomerRepository>();
+services.AddScoped<IContactPersonRepository, ContactPersonRepository>();
+services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+services.AddScoped<IRoleRepository, RoleRepository>();
+services.AddScoped<IServiceRepository, ServiceRepository>();
+services.AddScoped<IUnitTypeRepository, UnitTypeRepository>();
+services.AddScoped<IStatusTypeRepository, StatusTypeRepository>();
+
+services.AddScoped<IProjectService, ProjectService>();
+services.AddScoped<ICustomerService, CustomerService>();
+services.AddScoped<IContactPersonService, ContactPersonService>();
+services.AddScoped<IEmployeeService, EmployeeService>();
+services.AddScoped<IRoleService, RoleService>();
+services.AddScoped<IServiceService, ServiceService>();
+services.AddScoped<IUnitTypeService, UnitTypeService>();
+services.AddScoped<IStatusTypeService, StatusTypeService>();
+
+services.AddScoped<IMenuDialogs, MenuDialogs>();
+
+var serviceProvider = services.BuildServiceProvider();
+var menudialog = serviceProvider.GetRequiredService<IMenuDialogs>();
+
+menudialog.Run();
